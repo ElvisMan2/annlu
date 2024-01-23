c1d0ca82f3868ce25f311f51ac90236fb4377634
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2024-01-22 20:08:10 -0500

    form

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
 
-    function validarFormulario() {
-      var nombreApellido = document.getElementById('nombre-apellido').value;
-      var correo = document.getElementById('correo').value;
-      var telefono = document.getElementById('telefono').value;
-      var mensaje = document.getElementById('mensaje').value;
+function validarFormulario() {
+  var nombreApellido = document.getElementById('nombre-apellido').value;
+  var correo = document.getElementById('correo').value;
+  var telefono = document.getElementById('telefono').value;
+  var mensaje = document.getElementById('mensaje').value;
 
-      var errorMessage = '';
+  var errorMessage = '';
 
-      // Validar correo electrónico
-      var correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!correoRegex.test(correo)) {
-        errorMessage += 'Correo electrónico no válido.<br>';
-      }
+  // Validar que el mensaje no esté vacío
+  if (mensaje.trim() === '') {
+    errorMessage += 'El campo del mensaje no puede estar vacío.<br>';
+  }
 
-      // Validar teléfono
-      var telefonoRegex = /^\d{9}$/;
-      if (!telefonoRegex.test(telefono)) {
-        errorMessage += 'Teléfono debe tener 9 dígitos numéricos.<br>';
-      }
+  // Validar correo electrónico
+  var correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!correoRegex.test(correo)) {
+    errorMessage += 'Correo electrónico no válido.<br>';
+  }
 
-      // Mostrar mensaje de error o enviar formulario
-      var errorDisplay = document.getElementById('error-message');
-      if (errorMessage !== '') {
-        errorDisplay.innerHTML = errorMessage;
-      } else {
-        errorDisplay.innerHTML = '';
-        // Aquí puedes enviar el formulario, por ejemplo, mediante una petición AJAX
-        alert('Formulario enviado correctamente');
-      }
-    }
+  // Validar teléfono
+  var telefonoRegex = /^\d{9}$/;
+  if (!telefonoRegex.test(telefono)) {
+    errorMessage += 'Teléfono debe tener 9 dígitos numéricos.<br>';
+  }
+
+  // Mostrar mensaje de error o enviar formulario
+  var errorDisplay = document.getElementById('error-message');
+  if (errorMessage !== '') {
+    errorDisplay.innerHTML = errorMessage;
+  } else {
+    errorDisplay.innerHTML = '';
+    // Aquí puedes enviar el formulario, por ejemplo, mediante una petición AJAX
+    
+  }
+}
+
 
 
 
